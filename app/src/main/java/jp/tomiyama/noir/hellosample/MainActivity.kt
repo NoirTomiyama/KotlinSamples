1e0a9eabfc5209bfe574c617138cb341a64559a9
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-10-12 19:08:19 +0900

    クリアボタンの追加

--- app/src/main/java/jp/tomiyama/noir/hellosample/MainActivity.kt
+++ app/src/main/java/jp/tomiyama/noir/hellosample/MainActivity.kt
@@ -19,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     val listener = HelloListener()
     // 表示ボタンにリスナを設定
     btClick.setOnClickListener(listener)
+
+    // クリアボタンであるButtonオブジェクトを取得
+    val btClear = findViewById<Button>(R.id.btClear)
+    // クリアボタンにリスナを設定
+    btClear.setOnClickListener(listener)
   }
 
   // ボタンをクリックしたときのリスナクラス
@@ -28,10 +33,26 @@ class MainActivity : AppCompatActivity() {
       val input = findViewById<EditText>(R.id.etName)
       // メッセージを表示するTextViewオブジェクトの取得
       val output = findViewById<TextView>(R.id.tvOutput)
-      // 入力された名前文字列を取得
-      val inputStr = input.text.toString()
-      // メッセージの表示
-      output.text = inputStr + "さん、こんにちは！"
+
+      // idのR値に応じて処理を分岐
+      when (view.id) {
+        // 表示ボタンの場合
+        R.id.btClick -> {
+          // 入力された名前文字列を取得
+          val inputStr = input.text.toString()
+          // メッセージの表示
+          output.text = inputStr + "さん、こんにちは！"
+        }
+        // クリアボタンの場合
+        R.id.btClear -> {
+          // 名前入力欄を空文字に設定
+          input.setText("")
+          output.text = ""
+        }
+      }
+
+
     }
   }
+
 }
