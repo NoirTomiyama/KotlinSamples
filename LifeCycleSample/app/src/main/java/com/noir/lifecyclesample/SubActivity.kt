13ac4bbb64c56863ee4e8c3cbf070cb3bb6f1d5d
NoirTomiyama <lit.karu.noir@gmail.com>
2019-10-17 10:46:42 +0900

    all format

--- LifeCycleSample/app/src/main/java/com/noir/lifecyclesample/SubActivity.kt
+++ LifeCycleSample/app/src/main/java/com/noir/lifecyclesample/SubActivity.kt
@@ -1,12 +1,12 @@
 package com.noir.lifecyclesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SubActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sub)
+  }
 }
