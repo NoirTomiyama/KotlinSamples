aa24d0c1ac0eae3d7bc0130c8254ff3e139846dd
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-10-22 02:47:43 +0900

    HTTP接続

--- AsyncSample/app/src/main/java/jp/tomiyama/noir/asyncsample/MainActivity.kt
+++ AsyncSample/app/src/main/java/jp/tomiyama/noir/asyncsample/MainActivity.kt
@@ -8,6 +8,12 @@ import android.widget.ListView
 import android.widget.SimpleAdapter
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -66,19 +72,60 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  private fun is2String(stream: InputStream): String {
+    val sb = StringBuilder()
+    val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+    var line = reader.readLine()
+    while (line != null) {
+      sb.append(line)
+      line = reader.readLine()
+    }
+    reader.close()
+    return sb.toString()
+  }
+
   private inner class WeatherInfoReceiver() : AsyncTask<String, String, String>() {
     override fun doInBackground(vararg params: String): String {
       // 可変長引数の1個目(インデックス0)を取得．これが都市ID
       val id = params[0]
       val urlStr = "http://weather.livedoor.com/forecast/webservice/json/v1?city=${id}"
 
-      // ここに上記URLに接続してJSON文字列を取得する処理を記述
+      // URLオブジェクトを生成
+      val url = URL(urlStr)
+      // URLオブジェクトからHttpURLConnectionオブジェクトを取得
+      val con = url.openConnection() as HttpURLConnection
+      // http接続メソッドを設定
+      con.requestMethod = "GET"
+      // 接続
+      con.connect()
+      // HttpURLConnectionオブジェクトからレスポンスデータを取得
+      // 天気情報が格納されている
+      val stream = con.inputStream
+      // レスポンスデータであるInputSteamオブジェクトを文字列(JSON文字列)に変換
+      val result = is2String(stream)
+      // HttpURLConnectionオブジェクトを解放
+      con.disconnect()
+      // InputStreamオブジェクトを解放
+      stream.close()
+
       // JSON文字列を返す
       return result
     }
 
     override fun onPostExecute(result: String) {
-      // ここに天気情報JSON文字列を解析する処理を記述
+      // JSON文字列からJSONObjectオブジェクトを生成
+      // これをルートJSONオブジェクトとする
+      val rootJSON = JSONObject(result)
+      // ルートJSON直下の「description」JSONオブジェクトを取得
+      val descriptionJSON = rootJSON.getJSONObject("description")
+      // 「description」プロパティ直下の「text」文字列(天気概況文)を取得
+      val desc = descriptionJSON.getString("text")
+      // ルートJSON直下の「forecasts」JSON配列を取得
+      val forecasts = rootJSON.getJSONArray("forecasts")
+      // 「forecasts」JSON配列の１つ目(インデックス0)のJSONオブジェクトを取得
+      val forecastNow = forecasts.getJSONObject(0)
+      // 「forecasts」１つ目のJSONオブジェクトから「telop」文字列(天気)を取得
+      val telop = forecastNow.getString("telop")
 
       // 天気情報用文字列をTextViewにセット
       val tvWeatherTelop = findViewById<TextView>(R.id.tvWeatherTelop)
